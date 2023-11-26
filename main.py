d25c5b63c3c369307f939f85d6255b18a015344c
Marius Schueller <mariuseschueller@gmail.com>
2023-11-26 10:56:06 -0600

    with emails getting sent out

--- main.py
+++ main.py
@@ -31,23 +31,23 @@ def getMatchups(giverLst, receiverLst):
         receiverNum = random.randint(0, len(receiverLst) - 1)
 
         # add a check here for if the only person left is the same person and then just restart the process
-        while giverLst[giverNum] == receiverLst[receiverNum]:
+        while giverLst[giverNum] == receiverLst[receiverNum] or personalInfo.personalRule(giverLst[giverNum], receiverLst[receiverNum]):
             giverNum = random.randint(0, len(giverLst) - 1)
             receiverNum = random.randint(0, len(receiverLst) - 1)
 
         matchups[giverLst[giverNum]] = receiverLst[receiverNum]  # inclusive
         giverLst.remove(giverLst[giverNum])
         receiverLst.remove(receiverLst[receiverNum])
-        print("Giver list size: " + str(len(giverLst)) + "\nReceiver list size: " + str(len(receiverLst)))
+        # print("Giver list size: " + str(len(giverLst)) + "\nReceiver list size: " + str(len(receiverLst)))
 
-        if len(giverLst) == 1 and len(receiverLst) == 1 and giverLst[0] == receiverLst[0]:
+        if len(giverLst) == 1 and len(receiverLst) == 1 and (giverLst[0] == receiverLst[0] or personalInfo.personalRule(giverLst[0], receiverLst[0])):
             print("FAILED")
             return False
 
     return matchups
 
 
-# Not the most efficient solution, but works well to brute force in this case
+# Probably not the most efficient solution, but works well to brute force in this case
 res = False
 while not res:
     print("Trying to find a pairings")
@@ -70,22 +70,25 @@ def send_email(subject, body, sender, recipients, password):
     msg['To'] = ', '.join(recipients)
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
         smtp_server.login(sender, password)
-        smtp_server.sendmail(sender, personalInfo.testEmail, msg.as_string())  # FIXME hardcoded recipient to be me
-    print("Email sent!" + "\n")
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Email sent!")
 
 
 for pair in res:
     # Email details
     emailSubject = "ARMADA SECRET SANTA GIFT EXCHANGE!!!"
-    emailBody = "Dear " + personalInfo.nameKey[pair] \
-                + ",\nYour partner is: " + personalInfo.nameKey[res[pair]] \
-                + "\nYours truly,\nSantmada McBoaty"  # FIXME make sure to add $15 limit
+    emailBody = "Row row row, boats! \n\n"  \
+                + "I hope you are all ready to spread some holiday cheer! " \
+                  "Your secret Santa partner is none other than the amazing, wonderful: " \
+                + personalInfo.nameKey[res[pair]] \
+                + ".\n\nThe spending limit on the gift is $15.\n\n" \
+                + "Presents will be given at the last practice before finals week/break starts! Happy hunting!" \
+                  "\n\nStay chilly,\nSantmada McBoaty"
     emailSender = personalInfo.senderAddress
     emailRecipients = pair
     emailPassword = personalInfo.senderPassword
 
-    # FIXME COMMENT THE FOLLOWING PRINT STATEMENT
-    print(emailBody + "\nSENDING EMAIL TO: " + emailRecipients)  # To check message, but it's a secret so commented
-    # send_email(emailSubject, emailBody, emailSender, emailRecipients, emailPassword)  # FIXME UNCOMMENT TO SEND EMAILS
-
+    # So that it's a secret
+    # print(emailBody + "\nSENDING EMAIL TO: " + emailRecipients)
+    send_email(emailSubject, emailBody, emailSender, emailRecipients, emailPassword)
 print("Finished sending out emails")
